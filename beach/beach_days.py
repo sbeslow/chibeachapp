dd1816cc7290bf3ebf0becfea9a8196aa6cdebf2
sbeslow <bezzy21@yahoo.com>
2017-07-19 07:31:56 -0500

    Formatting main page

--- beach/beach_days.py
+++ beach/beach_days.py
@@ -4,17 +4,24 @@ from datetime import datetime, timedelta
 def find_failed_tests(beach_days):
     # Get the last five days
     today = datetime.now().date()
-    print(datetime.now())
     five_days_ago = today - timedelta(days=7)
     failed_tests = beach_days.loc[(beach_days['date'] >= five_days_ago) &
                                   (beach_days['dna_reading_mean'] >= 1000)]
     failed_tests = failed_tests.sort_values(['date', 'dna_reading_mean'], ascending=False)
     failed_tests['dna_reading_mean'] = failed_tests['dna_reading_mean']\
         .apply(lambda x: "%.2f" % x + ' cfu')
-    print(today)
-    print(failed_tests['date'])
     failed_tests['is_today'] = failed_tests['date'] == today
     return failed_tests.to_dict('records')
+
+
+def find_total_stats(beach_days):
+    samples = beach_days.loc[~beach_days['dna_reading_mean'].isnull()]
+    predictions = samples.loc[~samples['predicted_level'].isnull()]
+
+    return {'num_samples': len(samples),
+            'failed_samples': len(samples.loc[samples['dna_reading_mean'] >= 1000]),
+            'predicted_failed': len(predictions.loc[predictions['predicted_level'] >= 1000])
+            }
 
 
 def get_scatter_plot(this_beach):
@@ -38,23 +45,3 @@ def get_scatter_plot(this_beach):
 
     ret_val = {'readings': readings, 'predictions': predictions}
     return ret_val
-
-
-def get_scatter_plot_old(this_beach):
-    this_beach['dna_reading_mean'] = this_beach['dna_reading_mean']\
-        .apply(lambda x: float("%.2f" % x))
-    this_beach = this_beach.set_index('date')
-    date_iter = this_beach.index.min()
-    today = datetime.now().date()
-    this_beach = this_beach.to_dict('index')
-    scatter_points = []
-    x_ticks = 0
-    while date_iter <= today:
-        dna_reading = this_beach[date_iter]['dna_reading_mean'] if date_iter in this_beach else None
-        predicted_level = this_beach[date_iter]['predicted_level'] if date_iter in this_beach else None
-        scatter_points.append({'date': date_iter, 'dna_reading': dna_reading, 'predicted_level': predicted_level})
-        x_ticks += 1
-        date_iter = date_iter + timedelta(days=1)
-
-    scatter_plot = {'points': scatter_points, 'x_ticks': 5, 'y_ticks': 10}
-    return scatter_plot
--- beach/views.py
+++ beach/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from beach.chi_portal import get_beach_days_2017
-from beach.beach_days import find_failed_tests, get_scatter_plot
+from beach.beach_days import find_failed_tests, get_scatter_plot, find_total_stats
 
 
 def index(request):
     beach_days = get_beach_days_2017()
-    ret_val = {'beaches': list(beach_days['beach_name'].unique())}
+    ret_val = {'beaches': sorted(list(beach_days['beach_name'].unique())),
+               'stats': find_total_stats(beach_days)}
     ret_val['failed_tests'] = find_failed_tests(beach_days)
+
     return render(request, 'index.html', ret_val)
 
 
@@ -14,7 +16,7 @@ def show_beach(request, beach_name):
     beach_days = get_beach_days_2017()
     this_beach = beach_days.loc[beach_days['beach_name'] == beach_name]
 
-    ret_val = {'beaches': list(beach_days['beach_name'].unique())}
+    ret_val = {'beaches': sorted(list(beach_days['beach_name'].unique()))}
 
     predictions = this_beach.loc[~this_beach['predicted_level'].isnull()]
     ret_val['beach'] = {'beach_name': beach_name, 'samples_taken': len(this_beach),
